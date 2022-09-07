d171999a4d3241a38de06355f582d09ae6da1fdd
tree4553 <tree4553@naver.com>
2022-09-07 12:16:30 +0900

    RESTApi를 이용한 조회, 추가, 수정, 삭제 기능 추가

--- src/main/java/com/example/Board/repository/BoardRepository.java
+++ src/main/java/com/example/Board/repository/BoardRepository.java
@@ -3,5 +3,10 @@ package com.example.Board.repository;
 import com.example.Board.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository <Board, Long>{
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
