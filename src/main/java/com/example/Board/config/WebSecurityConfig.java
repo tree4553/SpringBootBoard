1817ec24ba3bbb6826cea83f1185c31a6238627f
tree4553 <tree4553@naver.com>
2022-09-10 23:04:09 +0900

    temp

--- src/main/java/com/example/Board/config/WebSecurityConfig.java
+++ src/main/java/com/example/Board/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ import javax.sql.DataSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     private DataSource dataSource;
-
+//temp
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
