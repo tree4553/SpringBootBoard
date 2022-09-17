72a3ba0bc9d3c4f981828996cfe27fee0e350774
tree4553 <tree4553@naver.com>
2022-09-17 22:59:58 +0900

    user, role 추가

---
+++ src/main/java/com/example/Board/model/Role.java
@@ -0,0 +1,18 @@
+package com.example.Board.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+}
